396e69a72c48ff970118d731bfd7ff2495f6da75
Brian Chan <b123400@gmail.com>
2016-05-01 23:03:34 +0800

    Upgrade Crashlytics, better exception catch

--- Xliffie/AppDelegate.m
+++ Xliffie/AppDelegate.m
@@ -19,6 +19,7 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    [[NSUserDefaults standardUserDefaults] registerDefaults:@{ @"NSApplicationCrashOnExceptions": @YES }];
     [Fabric with:@[[Crashlytics class]]];
 }
 
