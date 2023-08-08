b3894548e63cd8fd6e664841bcfdeabe36e7c192
b123400 <i@b123400.net>
2023-08-08 22:50:02 +0900

    Make suggestion selected on hover like in menu

---
+++ Xliffie/Suggestions/SuggestionTableView.m
@@ -0,0 +1,44 @@
+//
+//  SuggestionTableView.m
+//  Xliffie
+//
+//  Created by b123400 on 2023/08/08.
+//  Copyright © 2023 b123400. All rights reserved.
+//
+
+#import "SuggestionTableView.h"
+
+@implementation SuggestionTableView {
+    NSTrackingRectTag trackingTag;
+}
+
+
+- (void)awakeFromNib
+{
+    [[self window] setAcceptsMouseMovedEvents:YES];
+    trackingTag = [self addTrackingRect:[self frame] owner:self userData:nil assumeInside:NO];
+}
+
+- (void)dealloc
+{
+    [self removeTrackingRect:trackingTag];
+}
+
+- (void)mouseMoved:(NSEvent*)theEvent
+{
+    NSPoint point = [self convertPoint:[theEvent locationInWindow] fromView:nil];
+    NSInteger row = [self rowAtPoint:point];
+    if (row == -1) {
+        [self selectRowIndexes:[NSIndexSet indexSet] byExtendingSelection:NO];
+        return;
+    }
+    if (row != [self selectedRow]) {
+        [self selectRowIndexes:[NSIndexSet indexSetWithIndex:row] byExtendingSelection:NO];
+    }
+}
+
+- (void)mouseExited:(NSEvent *)event {
+    [self selectRowIndexes:[NSIndexSet indexSet] byExtendingSelection:NO];
+}
+
+@end
